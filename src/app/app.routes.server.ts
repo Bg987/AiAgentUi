4ee6749270077ef69258ca6b9eaddd63d47dc15e
Bg <bhavyagodhaniya2004@gmail.com>
2026-04-15 16:03:55 +0530

    fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'post/:id',
+    renderMode: RenderMode.Server // This tells Angular NOT to prerender it at build time
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
